3a04169e111fca1e19b72071396bb56486806f19
dazaiz <48435401+dazaiz@users.noreply.github.com>
2019-04-19 15:07:24 +0800

    new
    new

---
+++ template/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="zh" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./static/style.css">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
