73c56d31add1d394e81fc5765ea69b5c036c6732
dazaiz <48435401+dazaiz@users.noreply.github.com>
2019-05-23 16:54:09 +0800

    fix some bug

--- rename.php
+++ rename.php
@@ -6,9 +6,13 @@ $postfullpath = $_POST['postfullpath'];
 $newname = $_POST['newname'];
 $fullnewname = pdir($postfullpath).'/'.$newname;
 
+if((stripos($postfullpath,'./') === 0) || (stripos($postfullpath,'../')) || (stripos($postfullpath,'../') === 0) || (stripos($postfullpath,'..') === 0) || (stripos($postfullpath,'..'))){
+  echo '非法请求！';
+  exit;
+}
 
   if (rename($dirpath.$postfullpath,$dirpath.$fullnewname)) {
-    
+
 
       echo "<script>parent.parent.layer.closeAll();parent.parent.layer.msg('重命名成功，页面正在刷新');parent.parent.setTimeout('refresh()',2000);</script>";
 
